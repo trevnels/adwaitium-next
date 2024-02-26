adf3a6b0f5c581e7e96afa7d38ab3b479bcd5af7
trevnels <25140503+trevnels@users.noreply.github.com>
2024-02-26 01:00:39 -0500

    fix: new ublue-update config location

--- config/scripts/config-topgrade.sh
+++ config/scripts/config-topgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-sed -i '1s/^/[include]\npaths = ["\/etc\/ublue-os\/topgrade.toml"]\n\n/' /usr/etc/ublue-update/topgrade-user.toml && \
+sed -i '1s/^/[include]\npaths = ["\/etc\/ublue-os\/topgrade.toml"]\n\n/' /usr/share/ublue-update/topgrade-user.toml && \
     sed -i 's/min_battery_percent.*/min_battery_percent = 20.0/' /usr/etc/ublue-update/ublue-update.toml && \
     sed -i 's/max_cpu_load_percent.*/max_cpu_load_percent = 100.0/' /usr/etc/ublue-update/ublue-update.toml && \
     sed -i 's/max_mem_percent.*/max_mem_percent = 90.0/' /usr/etc/ublue-update/ublue-update.toml && \
